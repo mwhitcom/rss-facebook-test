2cf61261b6cf59b2ab2e6ea4608301d3a29e7f76
Matt Whitcomb <mwhitcom@gmail.com>
2017-08-08 12:38:08 -0700

    new links

--- controllers/markupController.js
+++ controllers/markupController.js
@@ -10,16 +10,31 @@ exports.createMarkup = (post) => {
 		</head>
 	  <body>
 	    <article>
-	      <header>
-	        <h1>${post.playlist_name}</h1>
+				<header>
 	        <figure>
 	          <img src="${post.image}" />
-	        </figure>
+					</figure>
+					<h1>${post.playlist_name}</h1>
+					<h2>
+						Stream On: 
+						<span><a href=${post.itunes_url}>iTunes | </a></span>
+						<span><a href=${post.itunes_url}>Spotify | </a></span>
+						<span><a href=${post.itunes_url}>Tidal | </a></span>
+						<span><a href=${post.itunes_url}>Amazon | </a></span>
+						<span><a href=${post.itunes_url}>Google | </a></span>
+						<span><a href=${post.itunes_url}>Youtube</a></span>
+					</h2>
 	        <time class="op-published" datetime=${date}>${date}</time>        
-	      </header>
-				<figure class="op-interactive">
-					<iframe src="https://calm-wildwood-87141.herokuapp.com/playlist/${post._id}" width="600" height="600"></iframe>
-				</figure>
+				</header>
+				<p>Song List</p>
+				<ol>
+					<li>Song Title</li>
+					<li>Song Title</li>
+					<li>Song Title</li>
+					<li>Song Title</li>
+					<li>Song Title</li>
+					<li>Song Title</li>
+				</ol>
 				<p>Playlist Radio</p>
 	    </article>
 	  </body>
