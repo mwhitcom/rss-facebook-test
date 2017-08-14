37026c81ab6d976254909c516b273c4615b3e175
Matt Whitcomb <mwhitcom@gmail.com>
2017-08-14 13:10:06 -0700

    layout

--- controllers/markupController.js
+++ controllers/markupController.js
@@ -9,15 +9,14 @@ exports.createMarkup = (post) => {
   		<meta property="op:markup_version" content="v1.0">
 		</head>
 	  <body>
-	    <article>
+	    <article>https://media.giphy.com/media/ICtmuwLLs1q48/giphy.gif
 				<header>
 	        <figure>
 	          <img src="${post.image}" />
 					</figure>
 					<h1>${post.playlist_name}</h1>
 					<h2>
-						<a href=${post.itunes_url}>iTunes |</a> <a href=${post.itunes_url}>Spotify |</a> <a href=${post.itunes_url}>Tidal |</a>
-						<a href=${post.itunes_url}>Amazon |</a> <a href=${post.itunes_url}>Google |</a> <a href=${post.itunes_url}>Youtube</a>
+						<a href=${post.itunes_url}>iTunes</a>|<a href=${post.itunes_url}>Spotify</a>|<a href=${post.itunes_url}>Tidal</a>|<a href=${post.itunes_url}>Amazon</a>|<a href=${post.itunes_url}>Google</a>|<a href=${post.itunes_url}>Youtube</a>
 					</h2>
 	        <time class="op-published" datetime=${date}>${date}</time>        
 				</header>
